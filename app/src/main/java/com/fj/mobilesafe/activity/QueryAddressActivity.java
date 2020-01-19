405ae5f035cfa72c83c37c8a2cbb2b3b7f8f0e66
fujie <1243596620@qq.com>
2020-01-19 23:46:19 +0800

    号码查询

--- app/src/main/java/com/fj/mobilesafe/activity/QueryAddressActivity.java
+++ app/src/main/java/com/fj/mobilesafe/activity/QueryAddressActivity.java
@@ -3,6 +3,8 @@ package com.fj.mobilesafe.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +12,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 import com.fj.mobilesafe.R;
+import com.fj.mobilesafe.engine.AddressDao;
 
 public class QueryAddressActivity extends Activity {
     private EditText et_phone;
@@ -25,6 +28,8 @@ public class QueryAddressActivity extends Activity {
         ;
     };
 
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,33 @@ public class QueryAddressActivity extends Activity {
         bt_query = (Button) findViewById(R.id.bt_query);
         tv_query_result = (TextView) findViewById(R.id.tv_query_result);
 
+        bt_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = et_phone.getText().toString();
 
+                if(!TextUtils.isEmpty(phone)){
+                    //2,查询是耗时操作,开启子线程
+                    query(phone);
+                }else{
+
+                }
+            }
+        });
+    }
+
+    /**
+     * 耗时操作
+     * 获取电话号码归属地
+     * @param phone	查询电话号码
+     */
+    protected void query(final String phone) {
+        new Thread(){
+            public void run() {
+                mAddress = AddressDao.getAddress(phone);
+                //3,消息机制,告知主线程查询结束,可以去使用查询结果
+                mHandler.sendEmptyMessage(0);
+            };
+        }.start();
     }
 }
