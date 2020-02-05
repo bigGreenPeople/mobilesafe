5d35169a1f82c50708889951d163b69d70dbd326
fujie123 <1243596620@qq.com>
2020-02-05 21:12:22 +0800

    应用管理功能完成

--- app/src/main/java/com/fj/mobilesafe/service/BlackNumberService.java
+++ app/src/main/java/com/fj/mobilesafe/service/BlackNumberService.java
@@ -5,10 +5,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.SmsMessage;
 import android.telephony.TelephonyManager;
+
 import com.android.internal.telephony.ITelephony;
 
 import androidx.annotation.Nullable;
@@ -22,6 +26,7 @@ public class BlackNumberService extends Service {
     private BlackNumberDao mDao;
     private TelephonyManager mTM;
     private MyPhoneStateListener mPhoneStateListener;
+    private MyContentObserver mContentObserver;
 
     @Override
     public void onCreate() {
@@ -45,7 +50,7 @@ public class BlackNumberService extends Service {
         super.onCreate();
     }
 
-    class MyPhoneStateListener extends PhoneStateListener{
+    class MyPhoneStateListener extends PhoneStateListener {
         //3,手动重写,电话状态发生改变会触发的方法
         @Override
         public void onCallStateChanged(int state, String incomingNumber) {
@@ -63,6 +68,7 @@ public class BlackNumberService extends Service {
             super.onCallStateChanged(state, incomingNumber);
         }
     }
+
     class InnerSmsReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -72,14 +78,14 @@ public class BlackNumberService extends Service {
             //2,循环遍历短信过程
             for (Object object : objects) {
                 //3,获取短信对象
-                SmsMessage sms = SmsMessage.createFromPdu((byte[])object);
+                SmsMessage sms = SmsMessage.createFromPdu((byte[]) object);
                 //4,获取短信对象的基本信息
                 String originatingAddress = sms.getOriginatingAddress();
                 String messageBody = sms.getMessageBody();
 
                 int mode = mDao.getMode(originatingAddress);
 
-                if(mode == 1 || mode == 3){
+                if (mode == 1 || mode == 3) {
                     //拦截短信(android 4.4版本失效	短信数据库,删除)
                     abortBroadcast();
                 }
@@ -91,10 +97,11 @@ public class BlackNumberService extends Service {
     public IBinder onBind(Intent arg0) {
         return null;
     }
+
     public void endCall(String phone) {
         int mode = mDao.getMode(phone);
 
-        if(mode == 2 || mode == 3){
+        if (mode == 2 || mode == 3) {
 //			ITelephony.Stub.asInterface(ServiceManager.getService(Context.TELEPHONY_SERVICE));
             //ServiceManager此类android对开发者隐藏,所以不能去直接调用其方法,需要反射调用
             try {
@@ -111,11 +118,35 @@ public class BlackNumberService extends Service {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+            //6,在内容解析器上,去注册内容观察者,通过内容观察者,观察数据库(Uri决定那张表那个库)的变化
+            mContentObserver = new MyContentObserver(new Handler(), phone);
+            getContentResolver().registerContentObserver(
+                    Uri.parse("content://call_log/calls"), true, mContentObserver);
         }
     }
+
+    class MyContentObserver extends ContentObserver {
+        private String phone;
+
+        public MyContentObserver(Handler handler, String phone) {
+            super(handler);
+            this.phone = phone;
+        }
+
+        //数据库中指定calls表发生改变的时候会去调用方法
+        @Override
+        public void onChange(boolean selfChange) {
+            //插入一条数据后,再进行删除
+            getContentResolver().delete(
+                    Uri.parse("content://call_log/calls"), "number = ?", new String[]{phone});
+            super.onChange(selfChange);
+        }
+    }
+
     @Override
     public void onDestroy() {
-        if(mInnerSmsReceiver!=null){
+        if (mInnerSmsReceiver != null) {
             unregisterReceiver(mInnerSmsReceiver);
         }
         super.onDestroy();
